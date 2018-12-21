c0063354c73bc40d625978ff813e7a9522800a81
aruyamam <aruyamam@gmail.com>
2018-12-21 14:46:45 +0900

    Break to Get Organized

---
+++ racing-game/Main.js
@@ -0,0 +1,41 @@
+var winWidth = window.innerWidth;
+var winHeight = window.innerHeight;
+
+var canvas, canvasContext;
+
+window.onload = function() {
+   canvas = document.getElementById('gameCanvas');
+   canvasContext = canvas.getContext('2d');
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight - 7;
+
+   var framesPerSecond = 30;
+   setInterval(updateAll, 1000 / framesPerSecond);
+
+   setupInput();
+
+   carImageLoad();
+
+   carReset();
+};
+
+function updateAll() {
+   moveAll();
+   drawAll();
+}
+
+function moveAll() {
+   carMove();
+
+   carTrackHnadling();
+}
+
+function clearScreen() {
+   colorRect(0, 0, canvas.width, canvas.height, 'black');
+}
+
+function drawAll() {
+   clearScreen();
+   carDraw();
+   drawTracks();
+}
